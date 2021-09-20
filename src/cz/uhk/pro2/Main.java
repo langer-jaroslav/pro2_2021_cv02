625f3f62adf4b8196bf6773eb124dfa3b8283fa6
Jaroslav Langer <langerjara@gmail.com>
2021-09-20 14:51:19 +0200

    ukazky datovych typu

--- src/cz/uhk/pro2/Main.java
+++ src/cz/uhk/pro2/Main.java
@@ -12,10 +12,26 @@ public class Main {
         menu.addItem(new PizzaMenuItem("sunkova", "sunka", 150));
         System.out.println("last update: " + menu.getLastUpdate());
 
+
+        PizzaMenu menu2 = menu;
+        menu2.addItem(new PizzaMenuItem("Polo", "kuřecí", 150));
+
         for (PizzaMenuItem item : menu.getItems()) {
             System.out.println(item);
         }
+        boolean b = true;
+        int i = 10;
+        float f = 1f;
+        double d = 2d;
+        int i2 = i;
 
+        i2++;
+        System.out.println(i);
+
+        if(i == i2){
+        }
+        if(menu == menu2){
+            System.out.println("pizza menu match");
+        }
     }
-
 }
