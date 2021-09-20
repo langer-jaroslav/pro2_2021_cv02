533f4989fb43a11c1f96fa608913b7ce9e7088f0
Jaroslav Langer <langerjara@gmail.com>
2021-09-20 14:05:29 +0200

    add PizzaMenuItem Class

---
+++ src/cz/uhk/pro2/models/PizzaMenuItem.java
@@ -0,0 +1,37 @@
+package cz.uhk.pro2.models;
+
+public class PizzaMenuItem {
+    private String name;
+    private String description;
+    private int price;
+
+    public PizzaMenuItem(String name,String description, int price ) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
